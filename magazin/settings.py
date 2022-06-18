93026ab8bcdca3255b64d9a2ca8c4bcbd18cd7a2
KMohirbek44 <komilovmohirbekfda1@gmail.com>
2022-06-18 13:11:52 +0500

    firstsettings11111111111111111

--- magazin/settings.py
+++ magazin/settings.py
@@ -81,6 +81,10 @@ TEMPLATES = [
     },
 ]
 
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+    # Add to this list all the locations containing your static files
+)
 WSGI_APPLICATION = 'magazin.wsgi.application'
 
 # Database
@@ -88,8 +92,13 @@ WSGI_APPLICATION = 'magazin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'de1nq3r5ur2ndl',
+        'USER':'uprrxsdymnjljd',
+        'PASSWORD':'3b7d0960ac67205b3a22581f6da4f4516dc0701359ede70ded4222624922f1b2',
+        'HOST':'ec2-3-234-131-8.compute-1.amazonaws.com',
+        'PORT':'5432',
+
     }
 }
 
@@ -154,5 +163,8 @@ MESSAGE_LEVEL = MY_INFO
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (STATIC_DIR, )
