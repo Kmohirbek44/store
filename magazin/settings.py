94f9c4b697b5ec39ea13d0c939fcbd7a480a2703
KMohirbek44 <komilovmohirbekfda1@gmail.com>
2022-06-18 12:15:45 +0500

    firstsettings111111111111

--- magazin/settings.py
+++ magazin/settings.py
@@ -160,6 +160,7 @@ MESSAGE_LEVEL = MY_INFO
 
 CART_SESSION_ID = 'cart'
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR, ]
 
