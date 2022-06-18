fc6a06fd4756818227adcead3dffce332a7511e4
KMohirbek44 <komilovmohirbekfda1@gmail.com>
2022-06-18 12:50:12 +0500

    firstsettings111111111111111

--- magazin/settings.py
+++ magazin/settings.py
@@ -161,8 +161,5 @@ MESSAGE_LEVEL = MY_INFO
 
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS=(
-    os.path.join(BASE_DIR,'static'),
-)
-
-
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (STATIC_DIR, )
