fecb12d014bd8d5fd6a49c6bc9030018411d076d
KMohirbek44 <komilovmohirbekfda1@gmail.com>
2022-06-18 12:04:46 +0500

    firstsettings11111111

--- magazin/settings.py
+++ magazin/settings.py
@@ -159,9 +159,19 @@ MESSAGE_LEVEL = MY_INFO
 
 
 CART_SESSION_ID = 'cart'
-STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS =os.path.join(BASE_DIR, 'static')
 
 
-STATIC_ROOT = BASE_DIR / 'mysite' / 'staticfiles'
+
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# custom
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR /  'static',
+]
